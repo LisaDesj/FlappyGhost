69d2a3ae00bcf2d2ed375ae6877e66dd84aef88a
zehouwu <tigerthepro@gmail.com>
2021-02-25 00:49:09 -0800

    implement render, needs debugging

--- FlappyGhost.hs
+++ FlappyGhost.hs
@@ -29,14 +29,41 @@ window = InWindow "FlappyGhost" (800, 400) (50, 50)
 background :: Color
 background = black
 
+
+data Pics = Pics{
+    land::Picture,
+    wall::Picture,
+    lamp::Picture,
+    ghost_norm::Picture,
+    ghost_lit::Picture,
+    ghost_invis::Picture
+}
+
+gameOver::Picture
+gameOver = translate (-300) 0 $ color white $ text "GameOver"
+
 {-
 Render
 takes the state and make the viasuals
 -}
--- render:: _____ -> Picture
--- temp render
--- render::GameState -> Picture
--- render = 
+render:: Pics -> GameState -> Picture
+render pics state
+    | (alive state) && (ghost state) = 
+        Pictures (renderHelper pics [(translate (-350) (-100) (ghost_invis pics))] (world state) 0)
+    | (alive state) =
+        Pictures (renderHelper pics [(translate (-350) (-100) (ghost_norm pics))] (world state) 0)
+    | otherwise = gameOver
+
+        
+
+renderHelper:: Pics -> [Picture] -> String -> Float -> [Picture]
+renderHelper pics ps [] x = ps
+renderHelper pics ps (h:t) x
+    | h == 'W' = 
+        (translate (-400 + (x * 50)) (-100) (wall pics)):(renderHelper pics ps t (x+1))
+    | h == 'L' =
+         (translate (-400 + (x * 50)) (-100) (lamp pics)):(renderHelper pics ps t (x+1))
+    |otherwise = renderHelper pics ps t (x+1)
 
 
 {-
@@ -101,16 +128,34 @@ main is where all the functions are put together and used via gloss
 main :: IO ()
 main = do
     -- load all the bmp images here
+    land <- loadBMP "images/land01.bmp"
+    wall <- loadBMP "images/wall01.bmp"
+    light <- loadBMP "images/light01.bmp"
+    ghost_norm <- loadBMP "images/ghost.bmp"
+    ghost_lit <- loadBMP "images/ghost_light.bmp"
+    ghost_invis <- loadBMP "images/ghost_invis.bmp"
+    sample <- loadBMP "images/sample.bmp"
     let wd = makeWorld 10 ""
+
     let state = GameState {
         world = wd,
         ghost = True,
         score = 0,
         alive = True
-   }
-
-    -- display window background drawing
+    }
+    let pics = Pics {
+        land = scale 800 80 land,
+        wall = scale 50 300 wall,
+        lamp = scale 50 80 light,
+        ghost_norm = scale 40 40 ghost_norm,
+        ghost_lit = scale 40 40 ghost_lit,
+        ghost_invis = scale 40 40 ghost_invis
+    }
     -- play window background 30 state render handleKeys update
 
-    -- moack
-    play window background 10 tempState tempRender tempHK tempUpdate
+    -- mock
+    -- play window background 10 tempState tempRender tempHK tempUpdate
+
+    -- display window background (scale 0.2 0.2 sample)
+    display window white (scale 2 2 ghost_lit)
+    -- play window background 50 tempState (render pics) tempHK tempUpdate
