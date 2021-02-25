96b87271ecce80d5f07139bf0a05f84a49fb5478
zehouwu <tigerthepro@gmail.com>
2021-02-25 12:33:58 -0800

    implemente check alive, needs debugging

--- FlappyGhost.hs
+++ FlappyGhost.hs
@@ -44,19 +44,22 @@ window = InWindow "FlappyGhost" (1000, 504) (50, 50)
 background :: Color
 background = black
 
-gameOver::Picture
-gameOver = translate (-300) 0 $ color white $ text "GameOver"
+gameOver:: String -> Picture
+gameOver score = Pictures
+                    [(translate 450 200 $ scale 0.3 0.3 $ color white $ text score),
+                    translate (-300) 0 $ color white $ text "GameOver"]
 
 
 -- Render
 -- takes the state and make the visuals
+-- 40 (25 pix width) cells on screen at a time
 render:: Pics -> GameState -> Picture
 render pics state
     | (alive state) && (ghost state) = 
         renderGame pics state (ghost_invis pics)
     | (alive state) =
         renderGame pics state (ghost_norm pics)
-    | otherwise = gameOver     
+    | otherwise = gameOver $ show (score state)     
 
 renderGame:: Pics -> GameState -> Picture -> Picture
 renderGame pics state ghost = 
@@ -72,9 +75,9 @@ renderHelper pics ps [] x = ps
 renderHelper pics ps (h:t) x 
     | x == -1 = (land pics):(renderHelper pics ps t (x+1))
     | h == 'W' = 
-        (translate (-400 + (x * 40)) 0 (wall pics)):(renderHelper pics ps t (x+1))
+        (translate (-400 + (x * 25)) 0 (wall pics)):(renderHelper pics ps t (x+1))
     | h == 'L' =
-         (translate (-400 + (x * 40)) 0 (lamp pics)):(renderHelper pics ps t (x+1))
+         (translate (-400 + (x * 25)) 0 (lamp pics)):(renderHelper pics ps t (x+1))
     |otherwise = renderHelper pics ps t (x+1)
 
 
@@ -82,9 +85,9 @@ renderHelper pics ps (h:t) x
 handleKeys
 takes the keystroke, the state, and updates the state
 -}
--- handleKeys Event -> GameState -> GameState
+handleKeys:: Event -> GameState -> GameState
 handleKeys (EventKey k ks _ _) gs
-    | SpecialKey KeySpace <- k = gs{ ghost = not (ghost gs), fade = 2}
+    | SpecialKey KeySpace <- k = gs{ ghost = True, fade = 5}
     | otherwise = gs
 handleKeys _ gs = gs
 
@@ -94,18 +97,35 @@ handleKeys _ gs = gs
 {-
 The function that changes the world after each iteration
 -}
-update f state = GameState {
-    world = uHelper (world state) [] 0,
-    ghost = if (fade state == 0) then False else True,
-    score = score state,
-    alive = True,
-    fade = if (fade state > 0) then (fade state) - 1 else 0
-}
+update::float -> GameState -> GameState
+update f state = 
+    if 
+        (alive state)
+    then 
+        GameState {
+        world = uHelper (world state) [] 0,
+        ghost = if (fade state == 0) then False else True,
+        score = score state,
+        alive = checkAlive (world state) (ghost state) 0,
+        fade = if (fade state > 0) then (fade state) - 1 else 0
+        }
+    else
+        state
 
 uHelper [] ns x = "E"
 uHelper (h:t) ns x
     |x == 0 = uHelper t ns (x+1)
     |otherwise = h:(uHelper t ns x)
+
+checkAlive::String -> Bool -> Int -> Bool
+checkAlive [] ghost count = True
+checkAlive (h:t) ghost count
+    | count < 11 = True && (checkAlive t ghost (count+1))
+    | count > 16 = True
+    | h == 'W' && ghost = True && (checkAlive t ghost (count+1))
+    | h == 'W' = False
+    | h == 'L' && ghost = False
+    | otherwise = True && (checkAlive t ghost (count+1))
 
 
 -- =======================
@@ -116,7 +136,7 @@ drawing = Pictures [translate (-100) (-100) $ color blue $ rectangleSolid 80 200
                     translate (100) (-100) $ color white $ rectangleSolid 80 200]
 
 tempState = GameState {
-    world = "EEEEEEELEEEEEEEEEEWEEEEEEEEEELEEEEEWEEEEEEEEEWEEEEELEEEEWEE",
+    world = "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEELEEEEEEEEEEEEEEEEEEEEEEEEEE",
     ghost = False,
     score = 0,
     alive = True,
@@ -151,11 +171,13 @@ main = do
     } 
     let pics = Pics {
         land = scale 1 1.5 land,
-        wall = scale 1 1.5 wall,
-        lamp = scale 1 1.5 light,
-        ghost_norm = translate (-100) 0 ghost_norm,
+        -- wall width = 100, lamp width = 200, ghost width = 50
+        -- wall 4 cells, lamp = 8 cells, ghost 2 cells
+        wall = scale 0.91 1.5 wall,
+        lamp = scale 1.14 1.5 light,
+        ghost_norm = translate (-175) 0 $ scale 0.714 0.714 ghost_norm,
         ghost_lit = ghost_lit,
-        ghost_invis = translate (-100) 0 ghost_invis
+        ghost_invis = translate (-175) 0 $ scale 0.714 0.714 ghost_invis
     }
     -- play window background 30 state render handleKeys update
 
